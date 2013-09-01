2ec56863ccf1e9d7ddb7c8dd271c80d9025ca3d6
Will Jessop <will@willj.net>
2013-09-02 00:56:33 +0100

    Make the basic example simpler

--- examples/example.go
+++ examples/example.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/wjessop/go-piglow"
-	"log"
-	"time"
 )
 
 func main() {
@@ -16,112 +14,30 @@ func main() {
 		log.Fatal("Couldn't create a Piglow: ", err)
 	}
 
-	// Fade in white
-	for i := 0; i <= 50; i++ {
-		p.SetWhite(uint8(i))
-		err = p.Apply()
-		if err != nil { // Apply the changes
-			log.Fatal("Couldn't apply changes: ", err)
-		}
-		time.Sleep(5e7)
-	}
+	// Set LED to brightness 10
+	p.SetLED(0, 10)
 
-	// Fade in blue, out white
-	for i := 0; i <= 50; i++ {
-		p.SetBlue(uint8(i))
-		p.SetWhite(uint8(50 - i))
-		err = p.Apply()
-		if err != nil { // Apply the changes
-			log.Fatal("Couldn't apply changes: ", err)
-		}
-		time.Sleep(5e7)
-	}
+	// Set LED to max brightness
+	p.SetLED(2, 255)
 
-	// Fade in green, out blue
-	for i := 0; i <= 50; i++ {
-		p.SetGreen(uint8(i))
-		p.SetBlue(uint8(50 - i))
-		err = p.Apply()
-		if err != nil { // Apply the changes
-			log.Fatal("Couldn't apply changes: ", err)
-		}
-		time.Sleep(5e7)
-	}
+	// Set all LEDs to brightness 10
+	p.SetAll(10)
 
-	// Fade in yellow, out green
-	for i := 0; i <= 50; i++ {
-		p.SetYellow(uint8(i))
-		p.SetGreen(uint8(50 - i))
-		err = p.Apply()
-		if err != nil { // Apply the changes
-			log.Fatal("Couldn't apply changes: ", err)
-		}
-		time.Sleep(5e7)
-	}
+	// Set the white LEDs to 15
+	p.SetWhite(15)
 
-	// Fade in orange, out yellow
-	for i := 0; i <= 50; i++ {
-		p.SetOrange(uint8(i))
-		p.SetYellow(uint8(50 - i))
-		err = p.Apply()
-		if err != nil { // Apply the changes
-			log.Fatal("Couldn't apply changes: ", err)
-		}
-		time.Sleep(5e7)
-	}
+	// Set the red LEDs to 20
+	p.SetRed(20)
 
-	// Fade in red, out orange
-	for i := 0; i <= 50; i++ {
-		p.SetRed(uint8(i))
-		p.SetOrange(uint8(50 - i))
-		err = p.Apply()
-		if err != nil { // Apply the changes
-			log.Fatal("Couldn't apply changes: ", err)
-		}
-		time.Sleep(5e7)
-	}
+	// Other functions are available for the other colours.
 
-	p.SetAll(0) // Turn them all off
+	// Set all LEDs on tentacle 0 to brightness 15
+	p.SetTentacle(0, 15)
 
-	p.SetLED(0, 255) // Set LED 0 to 255 (max brightness)
-	p.SetLED(1, 128) // Set LED 1 to half brightness
-	err = p.Apply()
-	if err != nil { // Apply the changes
-		log.Fatal("Couldn't apply changes: ", err)
-	}
+	// Set all LEDs on tentacle 2 to brightness 150
+	p.SetTentacle(2, 150)
 
-	time.Sleep(1e9) // Admire your handiwork
-
-	p.SetLED(3, 1) // Set LED 1 to half brightness
-
-	err = p.Apply()
-	if err != nil { // Apply the changes
-		log.Fatal("Couldn't apply changes: ", err)
-	}
-
-	time.Sleep(1e9) // Admire your handiwork
-
-	p.SetAll(0) // Turn all LEDs off
-	err = p.Apply()
-	if err != nil { // Apply the changes
-		log.Fatal("Couldn't apply changes: ", err)
-	}
-
-	time.Sleep(1e7)
-
-	p.SetAll(50) // Turn all LEDs on at 50, max would be 255. Ow, ow my eyes!
-	err = p.Apply()
-	if err != nil { // Apply the changes
-		log.Fatal("Couldn't apply changes: ", err)
-	}
-
-	// Fade out
-	for i := 50; i >= 0; i-- {
-		p.SetAll(uint8(i))
-		err = p.Apply()
-		if err != nil { // Apply the changes
-			log.Fatal("Couldn't apply changes: ", err)
-		}
-		time.Sleep(5e7)
-	}
+	// Display a value on a tentacle at brightness 10
+	// See code comments for more info on parameters
+	p.DisplayValueOnTentacle(0, 727.0, 1000.0, uint8(10), true)
 }
